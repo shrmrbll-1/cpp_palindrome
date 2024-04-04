2effda38f40c774382938e139a806fd761be013a
shrmrbll-1 <162495786+shrmrbll-1@users.noreply.github.com>
2024-04-04 19:17:18 +0800

    Update cpp_palindrome.cpp

--- cpp_palindrome.cpp
+++ cpp_palindrome.cpp
@@ -4,24 +4,23 @@
 using namespace std;
 
 bool is_palindrome(string input_string) {
-	size_t str_len = input_string.length() - 1;
+	size_t str_index = input_string.length();  
 
+	// will concatenate the element to these variables in each iteration. 
 	string new_string;
 	string reverse_string;
 
-	for (size_t i = (str_len); i <= (str_len); i--) {
-		char letter = tolower(input_string[i]);
-		if (letter != ' ' and isalpha(letter))
+	for (size_t i = (str_index); i > 0 ; i--) { //  used a decrement here as I will be pointing the last element first and move backwards 
+		char letter = tolower(input_string[i-1]); // initialize a variable letter to store the element, but we must convert the  element to lowercase as it might return false if we keep its true capitalization. 
+		if (letter != ' ' and isalpha(letter)) // check if the element is a space and an alphanumeric. We need to exclude punctuation and spaces
 		{
-			new_string += letter;
-			reverse_string = letter + reverse_string;
+			
+			new_string += letter; 
+			reverse_string = letter + reverse_string; 
 		}
 	}
-
-	return new_string == reverse_string;
-
+	return new_string == reverse_string; //  compare the new_string and reverse_string
 }
-
 
 int main()
 {
